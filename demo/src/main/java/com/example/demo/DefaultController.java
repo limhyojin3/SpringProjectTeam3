1a6bcfa482afc8e13e3897a590dac7340cf90452
hyojin3 <skwlaks1@gmail.com>
2026-04-17 02:12:34 +0900

    second commit

---
+++ demo/src/main/java/com/example/demo/DefaultController.java
@@ -0,0 +1,49 @@
+/*
+ * package com.example.demo;
+ * 
+ * import java.util.HashMap;
+ * 
+ * import org.springframework.stereotype.Controller; import
+ * org.springframework.ui.Model; import
+ * org.springframework.web.bind.annotation.RequestMapping; import
+ * org.springframework.web.bind.annotation.RequestMethod; import
+ * org.springframework.web.bind.annotation.RequestParam; import
+ * org.springframework.web.bind.annotation.ResponseBody;
+ * 
+ * import com.google.gson.Gson;
+ * 
+ * import jakarta.servlet.http.HttpServletRequest;
+ * 
+ * import jakarta.servlet.http.HttpServletRequest;
+ * 
+ * @Controller public class DefaultController {
+ * 
+ * @RequestMapping("/default.do") public String test(Model model) throws
+ * Exception{ return "/default"; }
+ * 
+ * @RequestMapping(value = "/default.dox", method = RequestMethod.POST, produces
+ * = "application/json;charset=UTF-8")
+ * 
+ * @ResponseBody public String login(Model model, @RequestParam HashMap<String,
+ * Object> map) throws Exception { HashMap<String, Object> resultMap = new
+ * HashMap<String, Object>();
+ * 
+ * return new Gson().toJson(resultMap); }
+ * 
+ * @RequestMapping("/board/edit.do") public String edit(HttpServletRequest
+ * request, @RequestParam HashMap<String, Object> map) throws Exception{
+ * System.out.println(map); request.setAttribute("boardNo", map.get("boardNo"));
+ * return "/board/board-edit"; }
+ * 
+ * @RequestMapping(value = "/board/list.dox", method = RequestMethod.POST,
+ * produces = "application/json;charset=UTF-8")
+ * 
+ * @ResponseBody public String list(Model model, @RequestParam HashMap<String,
+ * Object> map) throws Exception { HashMap<String, Object> resultMap = new
+ * HashMap<String, Object>(); System.out.println(map); //resultMap =
+ * boardService.getBoardList(map);
+ * 
+ * return new Gson().toJson(resultMap); }
+ * 
+ * }
+ */
