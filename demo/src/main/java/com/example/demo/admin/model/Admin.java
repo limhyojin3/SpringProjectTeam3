d90af1fc7746ac788fc6b14c677de223cf1c94b6
JH Choi <cjh900318@gmail.com>
2026-04-20 17:51:15 +0900

    feat: admin 페이지 헤더 푸터 적용. adminMain 대시보드 완성. adminReport, adminReviewWait 페이지 구현

--- demo/src/main/java/com/example/demo/admin/model/Admin.java
+++ demo/src/main/java/com/example/demo/admin/model/Admin.java
@@ -7,14 +7,37 @@ import lombok.Data;
 @Data
 public class Admin {
 
-	
-	//결제목록
+	// 리뷰목록
+	int reviewNo;
+	String userId;
+	String approvalStatus;
+	boolean isPaid;
+	String title;
+	String content;
+	String regDate;
+	String postDay;
+	int reviewWait;
+
+	// 신고목록
+	int reportNo;
+	String reporterId;
+	String targetType;
+	String uniTargetId;
+	String reportTitle;
+	String reportContent;
+	int answerStatus;
+	int actionStatus;
+	String reg_date;
+	String reportDay;
+	int reportWait;
+
+	// 결제목록
 	String saleMonth;
 	Long totalRevenue;
 	int orderCount;
-	
-	//회원목록
+
+	// 회원목록
 	String userMonth;
 	int userCount;
-	
+
 }
